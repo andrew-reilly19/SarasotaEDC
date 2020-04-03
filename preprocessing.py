d8b227265962c16d2df69e4d5c5117d2be9753de
andrew.reilly19 <andrew.reilly19@ncf.edu>
2020-04-03 12:37:20 -0400

    Initial code for the preprocessing doc

---
+++ preprocessing.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 17:58:26 2020
+
+@author: andrew
+
+This python file is meant to batch run the preprocessing on the .pdf files
+by merely taking the directory of the batch pdfs and:
+    1) convert each pdf to a png
+    2) rotate the documents clockwise, so the documents are rightside up
+    3) correct the skew of each page
+    4) output each page of the document individually into a separate directory
+    5) ... ?
+"""
